acb6871073003a1f702d76b0b9593d30c911fca2
cwallace033 <145495577+cwallace033@users.noreply.github.com>
2025-02-14 07:35:53 -0700

    still trying to fix it

--- routes/swagger.js
+++ routes/swagger.js
@@ -4,11 +4,14 @@ const swaggerDocument = require('../swagger.json');
 
 router.use('/api-docs', (req, res, next) => {
     res. setHeader ('Content-Type', 'text/html'); // Ensure correct content type next); 
+    if (req.path.endsWith('.css')) {
+        res.setHeader('Content-Type', 'text/css');
+      }
     next();
     }, swaggerUi.serve, swaggerUi.setup (swaggerDocument));
     
 
-router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
 
